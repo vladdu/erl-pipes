25a7fff9c72ba4421b3ca3ffc5f1e925f4e882b6
Vlad Dumitrescu <vladdu55@gmail.com>
2008-07-02 21:50:16 +0000

    added output process
    git-svn-id: https://erl-pipes.googlecode.com/svn/trunk@35 ea684b24-ca50-0410-a1ab-13a40b6da32f

--- src/pipe.erl
+++ src/pipe.erl
@@ -5,30 +5,31 @@
 %-compile([export_all]).
 
 new(Opts) ->
-    Receiver = spawn(fun() -> receiver(Opts) end),
-    Worker = spawn(fun() -> worker(Receiver, Opts) end),
-    {Receiver, Worker}.
+    Input = spawn(fun() -> input(Opts) end),
+    Output = spawn(fun() -> output(Opts) end),
+    _Worker = spawn(fun() -> worker(Input, Output, Opts) end),
+    {Input, Output}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -record(worker, {
-                 receiver,
-                 body=fun(X, S) -> {X, S} end,
-                 outputs=[],
-                 running=false,
-                 results=[]
+                 input,
+                 output,
+                 body=fun identity/2,
+                 running=false
                 }).
 
-worker(Receiver, Opts) ->
-    Receiver ! {config, worker, self()},
-    worker_loop(worker_state(Receiver, Opts)).
+identity(In, S) ->
+    {In, S, fun identity/2}.
 
-worker_state(Receiver, _Opts) ->
-    #worker{receiver=Receiver}.
+worker(Input, Output, Opts) ->
+    Input ! {config, worker, self()},
+    worker_loop(worker_state(Input, Output, Opts)).
 
-worker_loop(#worker{receiver=_Receiver,
-                    body=_Body,
-                    outputs=_Outs,
+worker_state(Input, Output, _Opts) ->
+    #worker{input=Input, output=Output}.
+
+worker_loop(#worker{body=_Body,
                     running=Running}=State)->
     State2 = case Running of
                  true ->
@@ -50,74 +51,131 @@ worker_config(State, _Cmd, _Args) ->
     State.
 
 execute(State) ->
+    %% fetch all required inputs
+    %% do the processing
+    %% send results
+
+    %% the state should be only what is required for the processing, not everything
+
     State.
 
 %%%%%%%%%%%%%%%%
 
--record(receiver, {
-                   buffer_data = [],
-                   stop_limit = 100,
-                   start_limit = 0,
-                   worker
-                  }).
--record(recbuffer, {
-                    pid,
-                    index,
-                    count=0,
-                    running=false
-                   }).
+-record(input, {
+                buffer_data = [],
+                stop_limit = 100,
+                start_limit = 1,
+                worker
+               }).
+-record(input_data, {
+                     pid,
+                     index,
+                     count=0,
+                     running=false
+                    }).
 
-receiver(Opts) ->
-    receiver_loop(receiver_state(Opts)).
+input(Opts) ->
+    input_loop(input_state(Opts)).
 
 get_opt(Name, Opts) ->
     {value, Val} = lists:keysearch(Name, 1, Opts),
     Val.
 
-receiver_state(Opts) ->
+input_state(Opts) ->
     BL = get_opt(buffers, Opts),
-    Buffers = lists:duplicate(lists:length(BL), #recbuffer{}),
-    Buffers1 = [R#recbuffer{index=I} || {R, I} <- lists:zip(Buffers, lists:seq(lists:length(Buffers)))],
-    #receiver{
-              buffer_data = Buffers1
-             }.
+    Buffers = lists:duplicate(lists:length(BL), #input_data{}),
+    Buffers1 = [R#input_data{index=I} || {R, I} <- lists:zip(Buffers, lists:seq(lists:length(Buffers)))],
+    #input{
+           buffer_data = Buffers1
+          }.
 
-receiver_loop(#receiver{
-                        buffer_data = Buffers,
-                        start_limit=Start,
-                        stop_limit=Stop,
-                        worker=worker
-                       }=State) ->
+input_loop(#input{
+                  buffer_data = Buffers,
+                  start_limit=Start,
+                  stop_limit=Stop,
+                  worker=worker
+                 }=State) ->
     receive
         {config, Cmd, Args} ->
-            State1 = receiver_config(State, Cmd, Args),
-            receiver_loop(State1);
+            State1 = input_config(State, Cmd, Args),
+            input_loop(State1);
         {data, From, Data} ->
-            {value, B, BRest} = lists:keytake(From, #recbuffer.pid, Buffers),
-            B1 = B#recbuffer{count=B#recbuffer.count+1},
-            worker ! {data, B#recbuffer.index, Data},
-            B2 = case (B#recbuffer.count >= Stop) and (B#recbuffer.running == true) of
+            {value, B, BRest} = lists:keytake(From, #input_data.pid, Buffers),
+            B1 = B#input_data{count=B#input_data.count+1},
+            worker ! {data, B#input_data.index, Data},
+            B2 = case (B#input_data.count >= Stop) and (B#input_data.running == true) of
                      true ->
-                         B#recbuffer.pid ! stop,
-                           B1#recbuffer{running=false};
+                         B#input_data.pid ! stop,
+                           B1#input_data{running=false};
                      false ->
                          B1
                  end,
-            receiver_loop(State#receiver{buffer_data=[B2|BRest]});
+            input_loop(State#input{buffer_data=[B2|BRest]});
         {fetch, Input} ->
-            {value, B, BRest} = lists:keytake(Input, #recbuffer.index, Buffers),
-            B1 = B#recbuffer{count=B#recbuffer.count-1},
-            B2 = case (B#recbuffer.count =< Start) and (B#recbuffer.running == false) of
+            {value, B, BRest} = lists:keytake(Input, #input_data.index, Buffers),
+            B1 = B#input_data{count=B#input_data.count-1},
+            B2 = case (B#input_data.count =< Start) and (B#input_data.running == false) of
                      true ->
-                         B#recbuffer.pid ! start,
-                           B1#recbuffer{running=true};
+                         B#input_data.pid ! start,
+                           B1#input_data{running=true};
                      false ->
                          B1
                  end,
-            receiver_loop(State#receiver{buffer_data=[B2|BRest]})
+            input_loop(State#input{buffer_data=[B2|BRest]})
     end.
 
-receiver_config(State, _Cmd, _Args) ->
+input_config(State, _Cmd, _Args) ->
+    State.
+
+
+%%%%%%%%%%%%%%%%
+
+-record(output, {
+                 outputs=[],
+                 worker,
+                 started=0
+                }).
+-record(outrec, {pid, index}).
+
+output(Opts) ->
+    output_loop(output_state(Opts)).
+
+output_state(_Opts) ->
+    #output{
+            }.
+
+output_loop(#output{
+                    worker=Worker,
+                    started=Started,
+                    outputs=Outputs
+                   }=State) ->
+    receive
+        {config, Cmd, Args} ->
+            State1 = output_config(State, Cmd, Args),
+            output_loop(State1);
+        stop ->
+            case length(Outputs) of
+                Started ->
+                    Worker ! stop
+            end,
+            output_loop(State#output{started=Started-1});
+        start ->
+            case length(Outputs)-1 of
+                Started ->
+                    Worker ! start
+            end,
+            output_loop(State#output{started=Started+1});
+        {result, Results} ->
+            Fun = fun({N, RL}) ->
+                          {value, Out} = lists:keysearch(N, #outrec.index, Outputs),
+                          [Out ! R || R<-RL],
+                          ok
+                  end,
+            lists:foreach(Fun, Results),
+            output_loop(State)
+    end.
+
+output_config(State, _Cmd, _Args) ->
     State.
 
 
