75b9139e76c741287048b2c46ca867c63ff5f877
Vlad Dumitrescu <vladdu55@gmail.com>
2008-07-03 14:14:18 +0000

    added some builtins
    git-svn-id: https://erl-pipes.googlecode.com/svn/trunk@49 ea684b24-ca50-0410-a1ab-13a40b6da32f

--- src/pipes.erl
+++ src/pipes.erl
@@ -5,11 +5,14 @@
 -export([
          new/1,
          connect/2,
-         wrap_simple_fun/1
+         wrap_simple_fun/1,
+         wait_result/1
         ]).
 
-new(Opts) ->
-    pipe:start(Opts).
+new(Fun) when is_function(Fun) ->
+    pipe:start([{body, wrap_simple_fun(Fun)}|default_opts()]);
+new(Opts) when is_list(Opts)  ->
+    pipe:start(Opts++default_opts()).
 
 connect({Pipe1, Output1}, {Pipe2, Input2}) ->
     Pipe1 ! {config, output, {Output1, {Pipe2, Input2}}},
@@ -21,3 +24,15 @@ connect(Pipe1, Pipe2) ->
 wrap_simple_fun(Fun) ->
     Fun.
 
+default_opts() ->
+    [].
+
+wait_result(Pipe) ->
+    Pipe ! {config, output, {default, {self(), default}}},
+    receive 
+        {data, default, Data} ->
+            Data
+    end.
+
+
+            
---
+++ src/pipes_builtins.erl
@@ -0,0 +1,67 @@
+-module(pipes_builtins).
+
+-compile([export_all]).
+
+%% do nothing
+null() ->
+    fun(_, State) -> 
+            {[], State} 
+    end.
+
+%% print to the console
+print(Fmt) ->
+    fun(X, State) -> 
+            io:format(Fmt, [X]), 
+            {[], State} 
+    end.
+
+print() ->
+    print("~p~n").
+
+%% read binary stream from a file
+binfile_in(FileName, Options) ->
+    fun(_X, init) ->
+            {ok, File} = file:open(FileName, [read_ahead, raw, binary | Options]),
+            {[], File};
+       (_X, File) ->
+            case file:read(File, 1024) of
+                {ok, Chunk} ->
+                    {Chunk, File};
+                eof ->
+                    file:close(File),
+                    {'$end', ok}
+            end
+    end.
+
+%% read Erlang terms from a file
+erl_file_in(FileName) ->
+    fun(_X, init) ->
+            {ok, Terms} = file:consult(FileName),
+            list_in(Terms)
+    end.
+
+%% get input from a list
+list_in([]) ->
+    fun(_X, _S) -> 
+            {'$end', ok}
+    end;
+list_in([H|T]) ->
+    Fun1 = fun(_X, [H_|T_]) -> 
+                   {H_, T_};
+              (_X, []) ->
+                   {'$end', ok}
+           end,
+    fun(_X, _S) -> 
+            {H, T, Fun1}
+    end.
+
+%% saves results into a list 
+list_out() ->
+    fun(_X, init) ->
+            {[], []};
+       ('$end', List) ->
+            {List, ok};
+       (X, List) ->
+            {[], [X|List]}
+    end.
+
